74bb40ee1df23d996696904d5a78ea16535d1f1e
JackGoldsworth <jackgoldsworth@gmail.com>
2019-05-02 21:45:37 -0500

    Finished

--- RegisterSwitch_ALU/RegisterSwitchorALU.v
+++ RegisterSwitch_ALU/RegisterSwitchorALU.v
@@ -10,9 +10,7 @@ module RegisterSwitchorALU(Perform, OP, K, a, b, c, d, R0, R1, R2, R3);
 	// Might need to add a carryout bit
 	
 	
-	always @(*)
-		begin
-		if(Perform == 1)
+	always @(OP or Perform)
 		begin
 			case(OP)
 				3'b000: begin
@@ -23,67 +21,60 @@ module RegisterSwitchorALU(Perform, OP, K, a, b, c, d, R0, R1, R2, R3);
 				end
 				3'b001: R0 = K;
 				3'b010: begin
-						begin
-							case(K)
-								2'b00: R0 = a;
-								2'b01: R0 = b;
-								2'b10: R0 = c;
-								2'b11: R0 = d;
-							endcase
-						end
+						case(K)
+							2'b00: R0 = a;
+							2'b01: R0 = b;
+							2'b10: R0 = c;
+							2'b11: R0 = d;
+						endcase
 				end
 				3'b011: begin
-					begin
 						case(K)
 							2'b00: R0 = a;
 							2'b01: R1 = a;
 							2'b10: R2 = a;
 							2'b11: R3 = a;
 						endcase
-					end
 				end
 				3'b100: begin
-					begin
 						case(K)
 							2'b00: R0 = a + a;
 							2'b01: R0 = a + b;
 							2'b10: R0 = a + b;
 							2'b11: R0 = a + b;
 						endcase
-					end
 				end
 				3'b101: begin
-					begin
 						case(K)
 							2'b00: R0 = a - a;
 							2'b01: R0 = a - b;
 							2'b10: R0 = a - c;
 							2'b11: R0 = a - d;
 						endcase
-					end
 				end
 				3'b110: begin
-					begin
 						case(K)
 							2'b00: R0 = a * a;
 							2'b01: R0 = a * b;
 							2'b10: R0 = a * c;
 							2'b11: R0 = a * d;
 						endcase
-					end
 				end
 				3'b111: begin
-					begin
 						case(K)
 							2'b00: R0 = 2 ** a;
 							2'b01: R0 = 2 ** b;
 							2'b10: R0 = 2 ** c;
 							2'b11: R0 = 2 ** d;
 						endcase
-					end
+				end
+				default: begin
+					R0 = 5'b00000;
+					R1 = 5'b00000;
+					R2 = 5'b00000;
+					R3 = 5'b00000;
 				end
 			endcase
-		end
 		end
 		
 endmodule
